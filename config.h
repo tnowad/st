376ddfdb6d88b55f6b3f0c93dc05bad7bfe9b697
Nguyen Minh Tuan <tnowad@gmail.com>
2024-06-11 16:21:45 +0700

    feat: change font to iosevka

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono NL:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Iosevka Term:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
